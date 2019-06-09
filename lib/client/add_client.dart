471f36705ca95c532004adda0473acaf526ca9f3
a <ruan65@gmail.com>
2019-06-09 15:19:05 +0300

    command line input

--- lib/client/add_client.dart
+++ lib/client/add_client.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:basic_api/proto/generated/lib/proto/service.pbgrpc.dart';
 import 'package:fixnum/fixnum.dart';
 import 'package:grpc/grpc.dart';
@@ -9,6 +12,7 @@ class Client {
   Future<void> main(List<String> args) async {
     //
     channel = ClientChannel('127.0.0.1',
+//    channel = ClientChannel('localhost',
         port: 1111,
         options: const ChannelOptions(
           credentials: const ChannelCredentials.insecure(),
@@ -17,16 +21,49 @@ class Client {
     stub = AddServiceClient(channel,
         options: CallOptions(timeout: Duration(seconds: 30)));
 
+    while (true) {
+      print('what to do?');
+      var action = stdin.readLineSync();
+
+      if (action == 'exit') {
+        await channel.shutdown();
+        return;
+      }
+
+      print('enter x');
+      var x = stdin.readLineSync();
+      print('enter y');
+      var y = stdin.readLineSync();
+
+      var additionRes = await (action == 'add'
+          ? addition(int.parse(x), int.parse(y))
+          : mult(int.parse(x), int.parse(y)));
+
+      print('result: ${additionRes ?? 'no result'}');
+    }
+  }
+
+  Future<Int64> addition(int a, int b) async {
     try {
       var result = await stub.add(Request()
-        ..x = Int64(500)
-        ..y = Int64(555));
-
-      print('got the result: $result');
+        ..x = Int64(a)
+        ..y = Int64(b));
+      return result.result;
     } catch (e) {
       print(e);
+      return null;
     }
+  }
 
-    await channel.shutdown();
+  Future<Int64> mult(int a, int b) async {
+    try {
+      var result = await stub.multiply(Request()
+        ..x = Int64(a)
+        ..y = Int64(b));
+      return result.result;
+    } catch (e) {
+      print(e);
+      return null;
+    }
   }
 }
